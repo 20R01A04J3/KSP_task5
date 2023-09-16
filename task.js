b9e311a6b38e80a00207691ef17d5b3edeaa46be
ANKAM ARUNRAJ <95906920+20R01A04J3@users.noreply.github.com>
2023-09-16 18:35:02 +0530

    Add files via upload

---
+++ task.js
@@ -0,0 +1,52 @@
+number1=20;
+number2=25;
+console.log(number1);
+console.log(number2);
+
+// arithemetic op + , - , * , / , % , **
+console.log(number1+number2);
+console.log(number1-number2);
+console.log(number1*number2);
+console.log(number1/number2);
+console.log(number1%number2);
+console.log(number1**number2);
+
+// assignments op  = , += , -= , =, /= %= *=
+
+console.log(number2=25);
+console.log(number2+=5);
+console.log(number2-=5);
+console.log(number2*=5);
+console.log(number2**=5);
+console.log(number2/=5);
+console.log(number2%=5);
+
+// comparsion op  == === != > < >= <= 
+console.log(number1==number2);
+console.log(number1===number2);
+console.log(number1!=number2);
+console.log(number1>number2);
+console.log(number1<number2);
+console.log(number1>=number2);
+console.log(number1<=number2);
+
+// string elements/methods
+
+let str="Hello,World"
+let str1="people"
+console.log(str.length);
+console.log(str.slice(0,5));
+console.log(str.substring(7,12));
+console.log(str.substr(7,5));
+console.log(str.replace("world","people"));
+console.log(str.replaceAll("o","0"));
+console.log(str.toUpperCase());
+console.log(str.toLowerCase());
+console.log(str.concat(str1));
+console.log(str.trim());
+
+
+
+
+
+
